57cee9b44193152498c2e0bf745dd4e928329fdd
robot <araxmatjonn@gmail.com>
2023-06-25 17:51:55 +0600

    installing home

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
-# Press the green button in the gutter to run the script.
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+    emit('my response', json)
+
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    emit('server_response', {'data': 'Welcome!'})
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    socketio.run(app, host='192.168.1.6', port=5000, allow_unsafe_werkzeug=True)
