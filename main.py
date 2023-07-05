c3aa2d61217ac8299cb7aff4fe60b32ea066cf83
Rahmatjon <araxmatjonn@gmail.com>
2023-07-05 09:01:55 -0400

    register successful

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ socketio = SocketIO(app)
 app.config['MYSQL_HOST'] = '83.136.233.166'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Aa@71077431'
-app.config['MYSQL_DB'] = 'taxi'
+app.config['MYSQL_DB'] = 'fsecure'
 
 mysql = MySQL(app)
 
@@ -58,10 +58,15 @@ def handle_my_custom_event(jsonn):
         # print('Registration successful!')
 
         msg = Message('Hello', sender='araxmatjonn@gmail.com', recipients=[email])
-        msg.body = "Your code: " + str(generate_five_digit_number())
+        code = generate_five_digit_number()
+        msg.body = "Your code: " + str(code)
         mail.send(msg)
         print("Message sent!")
         emit('register_response', {'message': 'Confirm your Email!'})
+        cur.execute('INSERT INTO confirm_email (email, code) VALUES (%s, %s)', (email, code))
+        mysql.connection.commit()
+        cur.close()
+        print('Registration successful!')
 
     # print('Get data from MySQL')
     # cur = mysql.connection.cursor()u
@@ -70,6 +75,24 @@ def handle_my_custom_event(jsonn):
     # for row in rows:
     #     print(f'{row}')
     # cur.close()
+
+
+@socketio.on('email_confirm')
+def handle_my_custom_event(jsonn):
+    print('received json: ' + str(jsonn))
+
+    json_obj = json.loads(jsonn)
+    email = json_obj['email']
+    email_code = json_obj['email_code']
+
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT code FROM confirm_email WHERE email = %s', email)
+    result = cur.fetchone()
+
+    if result is not None and result[0] == email_code:
+        emit('register_response', {'message': 'Error: Email already exists!'})
+
+    print('received data:', email, email_code)
 
 
 @socketio.on('connect')
