2eb3259e2f834be44da515541e281fed98cb57c4
Rahmatjon <araxmatjonn@gmail.com>
2023-08-08 02:55:01 -0400

    asd

---
+++ fsecure.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 08, 2023 at 02:52 AM
+-- Server version: 8.0.33-0ubuntu0.22.04.4
+-- PHP Version: 8.1.2-1ubuntu2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fsecure`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirm_email`
+--
+
+CREATE TABLE `confirm_email` (
+  `id` int NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_german2_ci NOT NULL,
+  `code` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_german2_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_german2_ci NOT NULL,
+  `token` varchar(50) COLLATE utf8mb4_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `token`) VALUES
+(3, 'asd@asd.asd', 'asdasd', '8ea7076289');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `confirm_email`
+--
+ALTER TABLE `confirm_email`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `confirm_email`
+--
+ALTER TABLE `confirm_email`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
